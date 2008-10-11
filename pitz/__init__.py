107a6e6ea0af6777e0e5a3eb84a1b73bf13a8c3f
W. Matthew Wilson <matt@tplus1.com>
2008-10-11 11:27:54 -0400

    Wrote the yaml.YAMLObject stuff

--- pitz/__init__.py
+++ pitz/__init__.py
@@ -1,1 +1,24 @@
-#
+# vim: set ts=4 sw=4 filetype=python:
+
+import yaml
+
+class Project(yaml.YAMLObject):
+    yaml_tag = u'ditz.rubyforge.org,2008-03-06/project'
+
+class Component(yaml.YAMLObject):
+    yaml_tag = u'ditz.rubyforge.org,2008-03-06/component'
+
+class Release(yaml.YAMLObject):
+    yaml_tag = u'ditz.rubyforge.org,2008-03-06/release'
+
+class Issue(yaml.YAMLObject):
+    yaml_tag = u'ditz.rubyforge.org,2008-03-06/issue'
+
+"""
+These are all the ditz types I know of.
+
+!ditz.rubyforge.org,2008-03-06/project 
+!ditz.rubyforge.org,2008-03-06/component 
+!ditz.rubyforge.org,2008-03-06/release 
+!ditz.rubyforge.org,2008-03-06/issue 
+"""
--- setup.py
+++ setup.py
@@ -6,8 +6,13 @@ version = '0.1'
 setup(name='pitz',
       version=version,
       description="Python implementation of ditz (ditz.rubyforge.org)",
+
       long_description="""\
-ditz (http://ditz.rubyforge.org) is the best distributed ticketing system that I know of.  It uses yaml to store all the information about the status of projects, so I'm going to try to write a python interface.""",
+ditz (http://ditz.rubyforge.org) is the best distributed ticketing
+system that I know of.  It uses yaml to store all the information about
+the status of projects, so I'm going to try to write a python
+interface.""",
+
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='ditz',
       author='Matt Wilson',
@@ -18,6 +23,7 @@ ditz (http://ditz.rubyforge.org) is the best distributed ticketing system that I
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+        'PyYAML'
           # -*- Extra requirements: -*-
       ],
       entry_points="""
