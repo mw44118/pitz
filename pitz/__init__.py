d122db0e110d8444944446e9e127a581047fad7d
W. Matthew Wilson <matt@tplus1.com>
2008-10-11 12:44:25 -0400

    Now I can at least parse a project.

--- pitz/__init__.py
+++ pitz/__init__.py
@@ -1,24 +1,64 @@
 # vim: set ts=4 sw=4 filetype=python:
 
+"""
+Python interface to ditz (http://ditz.rubyforge.org)
+"""
+
 import yaml
 
 class Project(yaml.YAMLObject):
-    yaml_tag = u'ditz.rubyforge.org,2008-03-06/project'
+    yaml_tag = u'!ditz.rubyforge.org,2008-03-06/project'
 
 class Component(yaml.YAMLObject):
-    yaml_tag = u'ditz.rubyforge.org,2008-03-06/component'
+    yaml_tag = u'!ditz.rubyforge.org,2008-03-06/component'
 
 class Release(yaml.YAMLObject):
-    yaml_tag = u'ditz.rubyforge.org,2008-03-06/release'
-
+    yaml_tag = u'!ditz.rubyforge.org,2008-03-06/release'
 class Issue(yaml.YAMLObject):
-    yaml_tag = u'ditz.rubyforge.org,2008-03-06/issue'
+    yaml_tag = u'!ditz.rubyforge.org,2008-03-06/issue'
 
-"""
-These are all the ditz types I know of.
+# Everything below here is test code.
 
-!ditz.rubyforge.org,2008-03-06/project 
-!ditz.rubyforge.org,2008-03-06/component 
-!ditz.rubyforge.org,2008-03-06/release 
-!ditz.rubyforge.org,2008-03-06/issue 
-"""
+__test__ = dict(
+    Project="""
+>>> import pitz, yaml
+
+>>> x  = yaml.load('''
+... --- !ditz.rubyforge.org,2008-03-06/project 
+... name: staffknex
+... version: "0.5"
+... components: 
+... - !ditz.rubyforge.org,2008-03-06/component 
+...   name: staffknex
+... releases: 
+... - !ditz.rubyforge.org,2008-03-06/release 
+...   name: 3.5.1
+...   status: :released
+...   release_time: 2008-10-10 18:40:45.054912 Z
+...   log_events: 
+...   - - 2008-09-02 19:16:28.409034 Z
+...     - Matthew Wilson <mw@staffknex.com>
+...     - created
+...     - Even more awesome than ever before
+...   - - 2008-10-10 18:40:45.054939 Z
+...     - Matthew Wilson <mw@staffknex.com>
+...     - released
+...     - ""
+... - !ditz.rubyforge.org,2008-03-06/release 
+...   name: Matt's queue
+...   status: :unreleased
+...   release_time: 
+...   log_events: 
+...   - - 2008-10-10 18:41:10.757638 Z
+...     - Matthew Wilson <mw@staffknex.com>
+...     - created
+...     - ""
+... ''')
+
+>>> x.name
+'staffknex'
+
+>>> len(x.releases)
+2
+""")
+
