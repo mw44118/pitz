22667866583365d1eb68cfcbaaf6faf9fea8d0fd
W. Matthew Wilson <matt@tplus1.com>
2009-01-30 14:41:47 -0500

    added a local ditz project so I can use ditz to track pitz issues (until it becomes self-hosting).

---
+++ pitz/tests/test_pitz.py
@@ -0,0 +1,16 @@
+# vim: set expandtab ts=4 sw=4 filetype=python:
+
+import pitz
+
+def test_thing_1():
+    """
+    Build a few things and a simple query to get the ones that have a
+    certain value for an attribute.
+    """
+    
+def test_tasks_and_milestones():
+
+    """
+    Make some tasks (subclasses of things) and then link a few of them
+    to some milestones (also thing subclasses).
+    """
