6e360a4207078cc4a314a8db8032cd76fa647128
W. Matthew Wilson <matt@staffknex.com>
2009-03-04 08:38:08 -0500

    Renamed and reorganized some files in junkyard.

--- pitz/junkyard/blob.py
+++ pitz/junkyard/blob.py
@@ -1,20 +1,69 @@
 # vim: set expandtab ts=4 sw=4 filetype=python:
 
-import pitz, pitz.scratch
+class Blob(yaml.YAMLObject):
 
-class Project(pitz.scratch.Blob):
+    @property
+    def id(self):
+        return id(self)
+
+    @property
+    def type(self):
+        return type(self)
+
+    def __new__(cls, *args, **kwargs):
+        """
+        I'm doing this so every instance can add itself to the list of
+        instances.
+
+        Remember that if I put the instances attribute on the class,
+        then all subclasses would get a reference to it.
+        """
+
+
+        if not hasattr(cls, 'instances'):
+            cls.instances = []
+
+        self = super(Blob, cls).__new__(cls)
+        cls.instances.append(self)
+        self.pointers_here = defaultdict(list)
+
+        return self
+
+    def __init__(self, **kwargs):
+        """
+        Any keyword params are added as traits.
+        """
+
+        for k,v  in kwargs.items():
+
+            setattr(self, k, v)
+
+            if isinstance(v, Blob):
+
+                plural_name = self.__class__.plural_name
+                v.pointers_here[plural_name].append(self)
+
+    def __repr__(self):
+
+        attributes = ', '.join(['%s=%s' % (k, v) 
+            for k, v in self.__dict__.items()
+            if k not in ('pointers_here', )])
+
+        return "%s(%s)" % (self.__class__.__name__, attributes)
+
+class Project(Blob):
     plural_name = 'projects'
 
-class Issue(pitz.scratch.Blob):
+class Issue(Blob):
     plural_name = 'issues'
 
-class Person(pitz.scratch.Blob):
+class Person(Blob):
     plural_name = 'people'
 
-class Milestone(pitz.scratch.Blob):
+class Milestone(Blob):
     plural_name = 'milestones'
     
-class Component(pitz.scratch.Blob):
+class Component(Blob):
     plural_name = 'components'
 
 def test_blob_1():
