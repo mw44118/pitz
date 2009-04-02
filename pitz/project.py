5564b82328e8b967fc595e40056a0e6e789de089
W. Matthew Wilson <matt@tplus1.com>
2009-04-01 22:40:54 -0400

    Ripped out some fancy stuff from project.Project.

--- pitz/project.py
+++ pitz/project.py
@@ -9,38 +9,12 @@ class Project(Bag):
     the only thing that keeps references to every other thing.
     """
     
-    # TODO: Use some dynamic nonsense to automate creation
-    # of all these properties based on all subclasses of Entity in
-    # this project.
-    @property
-    def tasks(self):
-        b = self.matches_dict(type='task')
-        b.title ='Tasks in project %s' % self.title
-        return b
-
-    @property
-    def releases(self):
-        return self.matches_dict(type='release')
-
-    @property
-    def iterations(self):
-        return self.matches_dict(type='iteration')
-
-    @property
-    def people(self):
-        return self.matches_dict(type='person')
-
-    @property
-    def components(self):
-        return self.matches_dict(type='component')
-
-    # End of stuff to somehow figure out dynamically.
-
     def append(self, e):
         """
-        Do a regular append and also set the entity's project attribute
-        to this project.
+        Do a regular append and some other stuff too.
         """
 
         super(Project, self).append(e)
+
+        # Make sure the entity remembers this project.
         e.project = self
