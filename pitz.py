47593fae71deb378bd60a14d1b6f4a3a2bb98bf6
Matěj Cepl <mcepl@redhat.com>
2012-05-30 16:25:41 +0200

    Add at least a minimal exception handling (missing subcommand).

--- pitz.py
+++ pitz.py
@@ -3,6 +3,16 @@
 import sys
 import subprocess
 
+def _help():
+    subprocess.call(['pitz-help'])
+    sys.exit(1)
+
+if len(sys.argv) < 2:
+    _help()
+
 cmd = sys.argv[1]
 new_args = sys.argv[2:] or []
-subprocess.call(["pitz-%s" % cmd] + new_args)
+try:
+    subprocess.call(["pitz-%s" % cmd] + new_args)
+except OSError as exc:
+    _help()
